41e5d2e657bc87987bb83eb0e8c6ac85ad0b4cf9
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-31 19:05:27 -0400

    innovation method works

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static('public'))
 
 // this is our body parser
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 
 
@@ -22,7 +23,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get("/budgets" , (req, res)=>{
     res.render("index.ejs" , {
-        allBudget: budget
+        allBudget: budget,
         // budget needs to be redeclared so index.ejs can access it
     })
 })
@@ -63,3 +64,47 @@ app.listen(port, ()=>{
     console.log(`Let's make some money on port ${port}`)
     // console.log(budget)
 })
+
+// <!-- CODE GRAVE YARD FOR INNOVATION SECTION -->
+
+// <!-- adding bankAccount variable to collect the total number of dollars in account -->
+// <!-- using squids and flounders since we are typing js in an ejs file -->
+// <!-- first lets declare the variable -->
+// <!-- my mind immediately went to the reduce array iterator, so i will revisit that lesson -->
+// <!-- before I can reduce though I need to filter just the cost of the items. s/o to project one for forcing me to use the find method! -->
+
+// let dollars = budget.find(function (amount){
+//     return amount.amount;
+// })
+
+// lol jk find won't work
+// gunna try filter
+// trying to get the amounts of each item dynamically and then reducing the new array...
+// find and filter couldn't work, going to make a new array with a loop
+// after playing around, i found this variable needs to be a blank array outside the for loop
+// let dollars = []
+
+// for (let i = 0; i < budget.length; i ++){
+//     dollars.push(budget[i].amount) // pushes all amounts to a new array
+//     console.log(dollars) // used this for debug, it works
+//     // able to console log all dollar amount
+//     // let sum = dollars.reduce(function (acc, num){
+//     //     return acc + num;
+//     // }, 0)
+// }
+
+// console.log(dollars) // dollars array now has all values and should be dynamic
+// // NOW let's try to reduce it
+// let bankAccount = dollars.reduce(function (acc, num){
+//     return acc + num;
+// }, 0)
+
+// console.log(bankAccount)
+// IT WORKS!! Reduces correctly
+// just need to figute out where to put it to get it to load on the webpage
+// let bankAccount = 
+
+// my method was working in console.log but not working in my actual web page
+//moved everything above to index.ejs and was still stuck
+// went to TA office hours and saw my way was more complicated then needed
+// was on the right track , but was able to simplify it.
