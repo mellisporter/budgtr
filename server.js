c46ea70a7f883909e81500cb278d7279113b12aa
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-31 15:07:59 -0400

    set up dependencies and database

---
+++ server.js
@@ -0,0 +1,33 @@
+// REQUIRE DEPENDENCIES
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// DATABASE
+
+const budget = require("./models/budget")
+
+// ROUTES
+
+// Index
+
+// News
+
+// Delete
+
+// Update
+
+// Create
+
+// Edit
+
+// Show
+
+
+// LISTENER
+
+app.listen(port, ()=>{
+    console.log(`Let's make some money on port ${port}`)
+    // console.log(budget)
+})
